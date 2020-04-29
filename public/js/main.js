ea5e7917f4282e7fc8450b79ec95b617ad334951
Sirius-Coder <eche17056@rgipt.ac.in>
2020-04-29 20:34:25 +0530

    Pre-final

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,50 @@
 const socket=io();
-
+var para=document.querySelector('.message-box');
+//GEt username and Room from the URL
+const{username,room}= Qs.parse(location.search,{
+  ignoreQueryPrefix:true
+})
+//Messge from server
 socket.on('message',message=>{
   console.log(message);
+  outputMessage(message);
+  para.scrollTop=para.scrollHeight;
 })
+//Send the username and Room name back to the createServer
+socket.emit('joinRoom',{username,room})
+$(document).ready(()=>{
+
+$(".message").keydown((e)=>{
+
+  if(e.keyCode=='13'){
+  socket.emit('chatMessage',$(".message").val());
+  $(".message").val("");//Clearing Input Field after Sending a message
+}
+
+
+})
+})
+
+//Get Room Users
+socket.on('roomUsers',({room,users})=>{
+  outputRoomName(room);
+  outputUsersName(users);
+})
+
+outputMessage=(message)=>{
+  var text=$("<p></p>").text(message.text).attr('id','ping');
+  var user=$("<h6></h6>").text(`Sent by ${message.username} at ${message.time}`).attr('id','user')
+  $(".message-box").append(text,user);
+}
+
+function outputRoomName(room){
+
+  $("#RoomName").html(room);
+}
+function outputUsersName(users) {
+  for(user in users){
+    console.log(user.username);
+  }
+
+
+}
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var http=require('http');
 var server=http.createServer(app);
 var socketIO=require("socket.io");
 const morgan=require('morgan');
+const formatMessage=require('../utils/message.js')
+const {userJoin,getCurrentUser,userLeave,getRoomUsers}=require('../utils/users.js')
 app.use(express.static(path.join(__dirname,'../public')));
 app.use(morgan("dev"));
 const io=socketIO(server);
@@ -16,8 +18,39 @@ app.get('/chatroom',(req,res)=>{
   res.sendFile('chatroom.html',{root:path.join(__dirname,'../public')})
 })
  io.on('connection',(socket)=>{
-   console.log('New Web Socket Connection established');
-   socket.emit('message',"Welcome to the application user");
+   socket.on("joinRoom",({username,room})=>{
+     const user=userJoin(socket.id,username,room);
+     socket.join(user.room);
+     //Broadcast the message that a new user has joined
+    socket.broadcast.to(user.room).emit('message',formatMessage('chatbot',`${user.username} has joined the chat`))
+  //Welcome the user
+     socket.emit('message',formatMessage('chatbot',`Welcome to RGIPT ChatBot ${user.username}`));
+    console.log( getRoomUsers(user.room));
+//Send the room and users to frontend
+io.to(user.room).emit('roomUsers',{
+  room:user.room,
+  users:getRoomUsers(user.room)
+})
+   })
+
+   //Listen For Chat Message
+   socket.on('chatMessage',msg=>{
+     const user=getCurrentUser(socket.id)
+     io.to(user.room).emit('message',formatMessage(user.username,msg));
+   })
+   //Listen when the client has disconnected
+   socket.on('disconnect',()=>{
+     const user=userLeave(socket.id)
+     if(user){
+      io.to(user.room).emit('message',formatMessage('chatbot',`${user.username} has left the chat`))
+
+     //Send the room and users to frontend
+     io.to(user.room).emit('roomUsers',{
+       room:user.room,
+       users:getRoomUsers(user.room)
+     })
+}
+   })
  })
 
 server.listen(port,()=>{
